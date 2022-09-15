1783f5d4474abbd8ab894d83609066aa1c400d5c
Leandro J. Capdesuñer Garcia <leandrojc88@gmail.com>
2022-09-15 01:53:43 -0300

    models and shered componentes

---
+++ src/app/shared/services/auth.service.ts
@@ -0,0 +1,48 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+export const ACCESS_TOKEN = 'access_token';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  // store the URL so we can redirect after logging in
+  redirectUrl: string | null = null;
+
+  constructor() { }
+
+  login(token: string): void {
+    window.sessionStorage.removeItem(ACCESS_TOKEN)
+    window.sessionStorage.setItem(ACCESS_TOKEN, JSON.stringify(token))
+  }
+
+  logout(): void {
+    window.sessionStorage.removeItem(ACCESS_TOKEN)
+  }
+
+  isLoggedIn(): boolean {
+    const item = window.sessionStorage.getItem(ACCESS_TOKEN);
+    return Boolean(item);
+  }
+
+  getToken(): string {
+    const item = window.sessionStorage.getItem(ACCESS_TOKEN);
+    if (!item) return "";
+    return item;
+  }
+
+  getHeaders(addAuthorization = false): HttpHeaders {
+    let requestOptions = new HttpHeaders({
+      'Content-Type': 'application/json'
+    });
+
+    if (addAuthorization) {
+      requestOptions = requestOptions.append('Authorization', 'Bearer ' + this.getToken());
+    }
+
+    return requestOptions;
+  }
+
+}
