b1ecd0b72967a774f30946df7c71bd628036fc5a
Leandro J. Capdesuñer Garcia <leandrojc88@gmail.com>
2022-09-27 18:33:26 -0300

    test app-component

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,35 +1,50 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './shared/layout/footer/footer.component';
+import { HeaderComponent } from './shared/layout/header/header.component';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let compiled: HTMLElement;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule],
+      declarations: [AppComponent, HeaderComponent, FooterComponent],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+
+    fixture.detectChanges();
+    compiled = fixture.nativeElement;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
+    expect(component).toBeTruthy();
   });
 
-  it(`should have as title 'spotify-test-project'`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('spotify-test-project');
+  it('should  by header exist', () => {
+    const headerDebugElement = fixture.debugElement.query(
+      By.directive(HeaderComponent)
+    );
+
+    const headerElement: HeaderComponent = headerDebugElement.componentInstance;
+
+    expect(headerElement).toBeTruthy();
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('spotify-test-project app is running!');
+  it('should  by footer exist', () => {
+    const footerDebugElement = fixture.debugElement.query(
+      By.directive(FooterComponent)
+    );
+
+    const footerComponent: FooterComponent =
+      footerDebugElement.componentInstance;
+
+    expect(footerComponent).toBeTruthy();
   });
 });
